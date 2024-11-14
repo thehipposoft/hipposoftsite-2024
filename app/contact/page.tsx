8dadbfba9ef5d595ba275436f398f96a544c262a
Diego Borigen <diego@iddigital.com.au>
2024-11-15 08:52:17 +1100

    Small form changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,7 @@
 import ContactComponent from '@/components/Contact';
-import React from 'react';
 
 export default function Contact() {
     return (
-        <div>
-            <ContactComponent />
-        </div>
+        <ContactComponent />
     );
 }
